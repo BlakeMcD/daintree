2cc64984885455a4f01519c41dbcf63e32fcb2e6
root <root@DESKTOP-482253N.localdomain>
2021-10-30 14:04:59 +1100

    got store filter working

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,16 @@
 class ItemsController < ApplicationController
 
     def index
-        @items = Item.all
+        # @items = Item.all
+        @stores = Store.all #for the stores filter
+        
+        #filter the list based on user input
+        if !params[:store].blank?
+            @items = Item.by_store(params[:store])
+        else
+            @items = Item.all
+        end
+
+
     end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,8 @@
 class Item < ApplicationRecord
     belongs_to :store, optional:true
     belongs_to :user, optional:true
+
+    def self.by_store(store_id)
+        where(store: store_id)
+    end
 end
