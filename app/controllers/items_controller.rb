4dd73f167e62630d8101f6fc193bfcd61af47439
root <root@DESKTOP-482253N.localdomain>
2021-10-30 14:23:06 +1100

    got clothing type working

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,6 +7,14 @@ class ItemsController < ApplicationController
         #filter the list based on user input
         if !params[:store].blank?
             @items = Item.by_store(params[:store])
+        elsif !params[:category].blank?
+            if params[:category] == "Jeans"
+                @items = Item.jeans
+            elsif params[:category] == "Jackets"
+                @items = Item.jacket
+            else params[:category] == "Shirts"
+                @items = Item.shirt
+            end
         else
             @items = Item.all
         end
--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,16 @@ class Item < ApplicationRecord
     def self.by_store(store_id)
         where(store: store_id)
     end
+
+    def self.jeans
+        where(sub_category: "jeans")
+    end
+
+    def self.shirt
+        where(sub_category: "shirt")
+    end
+
+    def self.jacket
+        where(sub_category: "jacket")
+    end
 end
