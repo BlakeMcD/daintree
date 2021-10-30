e372742ec2c8db502c79dee2882eadce280f0a7f
root <root@DESKTOP-482253N.localdomain>
2021-10-30 12:02:39 +1100

    added address and items to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,35 +18,74 @@ Store.create(name: 'Kotn', description: 'From its farms to its factories, Kotn c
 
 #Item
 s1 = Store.find_by id:1
+s2 = Store.find_by id:2
+s3 = Store.find_by id:3
+
     # sub_category: jeans
-    new_item = s1.items.build(category: 'clothes', sub_category: 'jeans', stock: 4, description: 'blue jeans', size: 'S', gender: 'M')
-    new_item.save
+    new_item1s1ijeans = s1.items.build(category: 'clothes', sub_category: 'jeans', stock: 4, description: 'blue jeans', size: 'S', gender: 'M')
+    new_item1s1ijeans.save
+    new_item1s2ijeans = s1.items.build(category: 'clothes', sub_category: 'jeans', stock: 6, description:'blue jeans', size: 'M', gender: 'F')
+    new_item1s2ijeans.save
+    new_item1s3ijeans = s1.items.build(category: 'clothes', sub_category: 'jeans', stock: 6, description:'blue jeans', size: 'L', gender: 'M')
+    new_item1s3ijeans.save
 
+    new_item2s1ijeans = s2.items.build(category: 'clothes', sub_category: 'jeans', stock: 4, description: 'blue jeans', size: 'S', gender: 'F')
+    new_item2s1ijeans.save
+    new_item2s2ijeans = s2.items.build(category: 'clothes', sub_category: 'jeans', stock: 6, description:'blue jeans', size: 'M', gender: 'F')
+    new_item2s2ijeans.save
+    new_item2s3ijeans = s2.items.build(category: 'clothes', sub_category: 'jeans', stock: 6, description:'blue jeans', size: 'L', gender: 'M')
+    new_item2s3ijeans.save
 
-    # s1.items << new_item
+    new_item3s1ijeans = s3.items.build(category: 'clothes', sub_category: 'jeans', stock: 4, description: 'blue jeans', size: 'S', gender: 'M')
+    new_item3s1ijeans.save
+    new_item3s2ijeans = s3.items.build(category: 'clothes', sub_category: 'jeans', stock: 6, description:'blue jeans', size: 'M', gender: 'M')
+    new_item3s2ijeans.save
+    new_item3s3ijeans = s3.items.build(category: 'clothes', sub_category: 'jeans', stock: 6, description:'blue jeans', size: 'L', gender: 'F')
+    new_item3s3ijeans.save
     
-    # s1.items.create(category: 'clothes', sub_category: 'jeans', stock: 6, description:'blue jeans', size: 'M', gender: 'M')
-
-    # s1.items.create(category: 'clothes', sub_category: 'jeans', stock: 4, description:'blue jeans', size: 'S', gender: 'M')
-    # s1.items.create(category: 'clothes', sub_category: 'jeans', stock: 6, description:'blue jeans', size: 'M', gender: 'M')
-    # s2.items.create(category: 'clothes', sub_category: 'jeans', stock: 5, description:'blue jeans', size: 'L', gender: 'M')
-    # s2.items.create(category: 'clothes', sub_category: 'jeans', stock: 4, description:'blue jeans', size: 'S', gender: 'F')
-    # s3.items.create(category: 'clothes', sub_category: 'jeans', stock: 6, description:'blue jeans', size: 'M', gender: 'F')
-    # s3.items.create(category: 'clothes', sub_category: 'jeans', stock: 5, description:'blue jeans', size: 'L', gender: 'F')
     # #sub_category: shirt
-    # s2.items.create(category: 'clothes', sub_category: 'shirt', stock: 4, description:'green shirt', size: 'S', gender: 'M')
-    # s2.items.create(category: 'clothes', sub_category: 'shirt', stock: 6, description:'green shirt', size: 'M', gender: 'M')
-    # s3.items.create(category: 'clothes', sub_category: 'shirt', stock: 5, description:'green shirt', size: 'L', gender: 'M')
-    # s3.items.create(category: 'clothes', sub_category: 'shirt', stock: 4, description:'green shirt', size: 'S', gender: 'F')
-    # s1.items.create(category: 'clothes', sub_category: 'shirt', stock: 6, description:'green shirt', size: 'M', gender: 'F')
-    # s1.items.create(category: 'clothes', sub_category: 'shirt', stock: 5, description:'green shirt', size: 'L', gender: 'F')
+    new_item1s1ishirt = s1.items.build(category: 'clothes', sub_category: 'shirt', stock: 4, description:'green shirt', size: 'S', gender: 'F')
+    new_item1s1ishirt.save
+    new_item1s2ishirt = s1.items.build(category: 'clothes', sub_category: 'shirt', stock: 4, description:'green shirt', size: 'M', gender: 'F')
+    new_item1s2ishirt.save
+    new_item1s3ishirt = s1.items.build(category: 'clothes', sub_category: 'shirt', stock: 4, description:'green shirt', size: 'L', gender: 'M')
+    new_item1s3ishirt.save
+
+    new_item2s1ishirt = s2.items.build(category: 'clothes', sub_category: 'shirt', stock: 4, description:'green shirt', size: 'S', gender: 'F')
+    new_item2s1ishirt.save
+    new_item2s2ishirt = s2.items.build(category: 'clothes', sub_category: 'shirt', stock: 4, description:'green shirt', size: 'M', gender: 'M')
+    new_item2s2ishirt.save
+    new_item2s3ishirt = s2.items.build(category: 'clothes', sub_category: 'shirt', stock: 4, description:'green shirt', size: 'L', gender: 'F')
+    new_item2s3ishirt.save
+
+    new_item3s1ishirt = s3.items.build(category: 'clothes', sub_category: 'shirt', stock: 4, description:'green shirt', size: 'S', gender: 'M')
+    new_item3s1ishirt.save
+    new_item3s2ishirt = s3.items.build(category: 'clothes', sub_category: 'shirt', stock: 4, description:'green shirt', size: 'M', gender: 'F')
+    new_item3s2ishirt.save
+    new_item3s3ishirt = s3.items.build(category: 'clothes', sub_category: 'shirt', stock: 4, description:'green shirt', size: 'L', gender: 'M')
+    new_item3s3ishirt.save
+
     # # #sub_category: jacket
-    # s3.items.create(category: 'clothes', sub_category: 'jacket', stock: 4, description:'suede jacket', size: 'S', gender: 'M')
-    # s3.items.create(category: 'clothes', sub_category: 'jacket', stock: 6, description:'suede jacket', size: 'M', gender: 'M')
-    # s1.items.create(category: 'clothes', sub_category: 'jacket', stock: 5, description:'suede jacket', size: 'L', gender: 'M')
-    # s1.items.create(category: 'clothes', sub_category: 'jacket', stock: 4, description:'suede jacket', size: 'S', gender: 'F')
-    # s2.items.create(category: 'clothes', sub_category: 'jacket', stock: 6, description:'suede jacket', size: 'M', gender: 'F')
-    # s2.items.create(category: 'clothes', sub_category: 'jacket', stock: 5, description:'suede jacket', size: 'L', gender: 'F')
+    new_item1s1ijacket = s1.items.build(category: 'clothes', sub_category: 'jacket', stock: 4, description:'suede jacket', size: 'S', gender: 'F')
+    new_item1s1ijacket.save
+    new_item1s2ijacket = s1.items.build(category: 'clothes', sub_category: 'jacket', stock: 4, description:'suede jacket', size: 'M', gender: 'F')
+    new_item1s2ijacket.save
+    new_item1s3ijacket = s1.items.build(category: 'clothes', sub_category: 'jacket', stock: 6, description:'suede jacket', size: 'L', gender: 'M')
+    new_item1s3ijacket.save
+
+    new_item2s1ijacket = s2.items.build(category: 'clothes', sub_category: 'jacket', stock: 4, description:'suede jacket', size: 'S', gender: 'M')
+    new_item2s1ijacket.save
+    new_item2s2ijacket = s2.items.build(category: 'clothes', sub_category: 'jacket', stock: 4, description:'suede jacket', size: 'M', gender: 'F')
+    new_item2s2ijacket.save
+    new_item2s3ijacket = s2.items.build(category: 'clothes', sub_category: 'jacket', stock: 6, description:'suede jacket', size: 'L', gender: 'F')
+    new_item2s3ijacket.save
+
+    new_item3s1ijacket = s3.items.build(category: 'clothes', sub_category: 'jacket', stock: 4, description:'suede jacket', size: 'S', gender: 'F')
+    new_item3s1ijacket.save
+    new_item3s2ijacket = s3.items.build(category: 'clothes', sub_category: 'jacket', stock: 4, description:'suede jacket', size: 'M', gender: 'M')
+    new_item3s2ijacket.save
+    new_item3s3ijacket = s3.items.build(category: 'clothes', sub_category: 'jacket', stock: 6, description:'suede jacket', size: 'L', gender: 'F')
+    new_item3s3ijacket.save
 
 #Address
 u1 = User.find_by id:1
@@ -54,5 +93,11 @@ u2 = User.find_by id:2
 u3 = User.find_by id:3
 address_one = u1.build_address(street_address:'26 Martens Place', city: 'Tingalpa', state: 'QLD', postcode: 4173, address_type: 'home')
 address_one.save
+
+address_two = u2.build_address(street_address:'57 Bayview Close', city: 'Tieri', state: 'QLD', postcode: 4709, address_type: 'home')
+address_two.save
+
+address_three = u3.build_address(street_address:'35 Raglan Street', city: 'Tingoora', state: 'VIC', postcode: 3507, address_type: 'home')
+address_three.save
 # u2.address.create(street_address:'57 Bayview Close', city: 'Tieri', state: 'QLD', postcode: 4709, address_type: 'home')
 # u3.address.create(street_address:'35 Raglan Street', city: 'Tingoora', state: 'VIC', postcode: 3507, address_type: 'home')
