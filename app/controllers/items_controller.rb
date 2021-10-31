cc2567d0f5527c9c31f71101118427210ea4e1f1
root <root@DESKTOP-482253N.localdomain>
2021-10-31 11:14:05 +1100

    final changes before pull

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -44,51 +44,17 @@ class ItemsController < ApplicationController
         else
             @items = Item.all
         end
+    end
 
-        # if !params[:store].nil?
-        #     @@all_items = Item.store_is_selected(ps)
-        # end
+    def add_to_cart
+        @item = Item.find(params[:id])  #get the item from the path
 
-        # if !params[:category].blank?
-        #     @@all_items = Item.sub_category_is_selected(params[:category])
-        # end
+        #retrieve the cart, or if one doesn't exist, create one
+        cart = session[:cart] ||= []
+        cart << @item.id
 
-        # if !params[:size].blank?
-        #     @@all_items = Item.size_is_selected(params[:size])
-        # end
-
-        # if !params[:gender].blank?
-        #     @@all_items = Item.gender_is_selected(params[:gender])
-        # end
-
-
-
-
-
-
-
-        # #filter the list based on user input
-        # if !params[:store].blank?
-        #     # @items = Item.by_store(params[:store])
-        #     @items = Item.store_is_selected(params[:store])
-        # elsif !params[:category].blank?
-
-        #     @items = Item.sub_category_is_selected(params[:category])
-        #     # if params[:category] == "Jeans"
-        #     #     @items = Item.jeans
-        #     # elsif params[:category] == "Jackets"
-        #     #     @items = Item.jacket
-        #     # else params[:category] == "Shirts"
-        #     #     @items = Item.shirt
-        #     # end
-        # elsif !params[:size].blank?
-        #     @items = Item.size_is_selected(params[:size])
-        # elsif !params[:gender].blank?
-        #     @items = Item.gender_is_selected(params[:gender])
-        # else
-        #     @items = Item.all
-        # end
-
+        #Save the cart in the session
+        session[:cart] = cart
 
     end
 end
