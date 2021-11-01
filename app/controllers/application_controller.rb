2d9d7b0eb9d3119427c793154269a315032baa36
root <root@DESKTOP-482253N.localdomain>
2021-11-01 20:45:51 +1100

    got session and cart working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
     skip_before_action :verify_authenticity_token  #reminder - will have to look closer at this
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,11 @@ Rails.application.routes.draw do
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   post "/logout", to: "sessions#destroy"
+
+  #Add to cart
+  get 'about', to: 'items#about' #is this necessary??
+  post 'items/add_to_cart/:id', to: 'items#add_to_cart', as: 'add_to_cart'
+  delete 'items/remove_from_cart/:id', to: 'items#remove_from_cart', as: 'remove_from_cart'
   #STORE SESSION - Not active
   # get "/store/login", to: "sessions#new"
   # post "/store/login", to: "sessions#create"
