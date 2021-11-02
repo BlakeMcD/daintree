5b4edfad649d0ec2ad61bf4b4234a9016a5b03e1
root <root@DESKTOP-482253N.localdomain>
2021-11-02 21:58:17 +1100

    added new and edit controller code and views

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,6 +6,26 @@ class ItemsController < ApplicationController
     def about
     end
 
+    def new
+        @item = Item.new
+    end
+
+    def create
+        @item = Item.new(item_params)
+        @item.save
+        redirect_to items_path
+    end
+
+    def edit
+        @item = Item.find(params[:id])
+    end
+
+    def update
+        @item = Item.find(params[:id])
+        @item.update(item_params)
+        redirect_to items_path
+    end
+
     def index
 
         # @items = Item.all
@@ -82,6 +102,10 @@ class ItemsController < ApplicationController
 
     def load_cart
         @cart = Item.find(session[:cart])
+    end
+
+    def item_params
+        params.require(:item).permit(:name, :category, :sub_category, :stock, :price_cents, :description,:size, :gender, :img_url, :store_id)
     end
 
 end
